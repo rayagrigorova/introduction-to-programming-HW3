bf6373b55d6c32dd9eea899254be320f331dc3ff
rayagrigorova <72023155+rayagrigorova@users.noreply.github.com>
2022-12-20 23:11:10 +0200

    Add files via upload

--- fn5MI0600167_d3_3.cpp
+++ fn5MI0600167_d3_3.cpp
@@ -15,15 +15,17 @@
 
 const int HHMM = 4;
 
-//void printTwoDimArray(const char** const arr, int n, int m);
+//void printTwoDimArray(char** arr, int n, int m);
+void printTime(char* time);
 
-int compareTimes(const char* const time1, const char* const time2);
-const char* maximalTime(const char* const time1, const char* const time2);
-const char* minimalTime(const char* const time1, const char* const time2);
+int compareTimes(char* time1, char* time2);
 
-void calculateMinimalNumberOfSites(const char** const landingTimes, const char** const departureTimes, const int N);
-bool planesAreAtTheAirportAtTheSameTime(const char** const landingTimes, const char** const departureTimes, const int N, const int i, const int j);
-void evaluateBusiestPeriods(const char** const landingTimes, const char** const departureTimes, const int N, const int maxNumberOfPlanesOnAirport);
+void calculateMinimalNumberOfSites(char** landingTimes, char** departureTimes, const int N);
+void sortArrays(char** landingTimes, char** departureTimes, const int N);
+int calculateNumberOfIntervalsAPointIsIn(char** landingTimes, char** departureTimes, const int N, char* point);
+bool pointIsInRange(char* point, char* landingTime, char* departureTime);
+void evaluateMaxIntersectionIntervals(char** landingTimes, char** departureTimes, const int N, const int maxIntersectingCount);
+bool isInArray(char* element, char** arr, const int N);
 
 void freeMemory(char** arr, const int rows);
 
@@ -59,21 +61,11 @@ int main()
 	freeMemory(departureTimes, N);
 }
 
-//void printTwoDimArray(const char** arr, int n, int m) {
-//	for (int i = 0; i < n; i++) {
-//		std::cout << "\n";
-//		for (int j = 0; j < m; j++) {
-//			std::cout << arr[i][j];
-//		}
-//	}
-//	std::cout << "\n\n\n";
-//}
-
 //This function returns:
 // -> 1 if time1 > time2
 // -> 0 if time1 == time2
 // -> -1 if time1 < time2
-int compareTimes(const char* const time1, const char* const time2) {
+int compareTimes(char* time1, char* time2) {
 	for (int i = 0; i < HHMM; i++) {
 		if (time1[i] > time2[i]) {
 			return 1;
@@ -85,93 +77,115 @@ int compareTimes(const char* const time1, const char* const time2) {
 	return 0;
 }
 
-void calculateMinimalNumberOfSites(const char** const landingTimes, const char** const departureTimes, const int N) {
-	//My goal is to find the intersection 
-	//in order to find the busiest period at the airport 
-	//and from there - the minimal number of sites needed 
+void calculateMinimalNumberOfSites(char** landingTimes,  char** departureTimes, const int N) {
+	sortArrays(landingTimes, departureTimes, N);
 
-	int maxNumberOfPlanesOnAirport = 0;
-	int numberOfPlanesInGroup = 0;
-
-	//For each plane
+	//For each landingTime (and departureTime), I will calculate the number of intervals it is in
+	//The beginning of the max intersection is some landingTime and the end is some departureTime 
+	int maxIntersectionsStart = -1, maxIntersectionsEnd = -1;
+	int startIndex = -1, endIndex = -1;
 	for (int i = 0; i < N; i++) {
-		numberOfPlanesInGroup = 1;
-		for (int j = 0; j < N; j++) {
-			if (i == j) { continue; }
-			if (planesAreAtTheAirportAtTheSameTime(landingTimes, departureTimes, N, i, j)) {
-				numberOfPlanesInGroup++;
-			}
+		int intervalsLandingTimeIsIn = calculateNumberOfIntervalsAPointIsIn(landingTimes, departureTimes, N, landingTimes[i]);
+		int intervalsDepartureTimeIsIn = calculateNumberOfIntervalsAPointIsIn(landingTimes, departureTimes, N, departureTimes[i]);
+
+		if (intervalsLandingTimeIsIn > maxIntersectionsStart) {
+			maxIntersectionsStart = intervalsLandingTimeIsIn;
+			startIndex = i;
 		}
-		if (numberOfPlanesInGroup > maxNumberOfPlanesOnAirport) {
-			maxNumberOfPlanesOnAirport = numberOfPlanesInGroup;
-		}
-	}
-	std::cout << maxNumberOfPlanesOnAirport;
-	evaluateBusiestPeriods(landingTimes, departureTimes, N, maxNumberOfPlanesOnAirport);
-}
-
-bool planesAreAtTheAirportAtTheSameTime(const char** const landingTimes, const char** const departureTimes, const int N, const int i, const int j) {
-	//If the times of the second plane are between 
-	//the landing and departure times of the first, they are at the airport together. 
-	//This means that landingTimeSecond >= landingTimeFirst
-	//and landingTimeSecond <= departureTimeFirst
-	return((compareTimes(landingTimes[j], landingTimes[i]) >= 0)
-		&& (compareTimes(landingTimes[j], departureTimes[i]) <= 0));
-}
-
-const char* maximalTime(const char* const time1, const char* const time2) {
-	const char* result;
-	if (compareTimes(time1, time2) >= 0) {
-		result = time1;
-	}
-	else {
-		result = time2;
-	}
-	return result;
-}
-
-const char* minimalTime(const char* const time1, const char* const time2) {
-	const char* result;
-	if (compareTimes(time1, time2) >= 0) {
-		result = time2;
-	}
-	else {
-		result = time1;
-	}
-	return result;
-}
-
-void evaluateBusiestPeriods(const char** const landingTimes, const char** const departureTimes, const int N, const int maxNumberOfPlanesOnAirport) {
-	int numberOfPlanesInGroup = 0;
-	const char* periodStart = nullptr;
-	const char* periodEnd = nullptr;
-	for (int i = 0; i < N; i++) {
-		numberOfPlanesInGroup = 1;
-		periodStart = landingTimes[i];
-		periodEnd = departureTimes[i];
-		//For each plane
-		for (int j = 0; j < N; j++) {
-			if (i == j) { continue; }
-			if (planesAreAtTheAirportAtTheSameTime(landingTimes, departureTimes, N, i, j)) {
-				numberOfPlanesInGroup++;
-				periodStart = maximalTime(periodStart, landingTimes[j]);
-				periodEnd = minimalTime(periodEnd, departureTimes[j]);
-			}
-		}
-		if (numberOfPlanesInGroup == maxNumberOfPlanesOnAirport) {
-			std::cout << "\n";
-			std::cout << periodStart << "-" << periodEnd;
+		if (intervalsDepartureTimeIsIn > maxIntersectionsEnd) {
+			maxIntersectionsEnd = intervalsDepartureTimeIsIn;
+			endIndex = i;
 		}
 	}
+	std::cout << maxIntersectionsStart << "\n";
+	evaluateMaxIntersectionIntervals(landingTimes, departureTimes, N, maxIntersectionsStart);
 }
 
 void freeMemory(char** arr, const int rows) {
-	//For each dub-array
+	//For each sub-array
 	for (int i = 0; i < rows; i++) {
 		delete[] arr[i];
 	}
 	delete[] arr;
 }
 
+void sortArrays(char** landingTimes, char** departureTimes, const int N) {
+	//The planes must be sorted in ascending order for the algorithm to work
+	//For this purpose, I will not copy the char arrays in landingTimes and departureTimes,
+	//instead I will simply change the pointers
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < N - i - 1; j++) {
+			if (compareTimes(landingTimes[j], landingTimes[j + 1]) > 0) {
+				char* ptr = landingTimes[j];
+				landingTimes[j] = landingTimes[j + 1];
+				landingTimes[j + 1] = ptr;
 
+				ptr = departureTimes[j];
+				departureTimes[j] = departureTimes[j + 1];
+				departureTimes[j + 1] = ptr;
+			}
+		}
+	}
+}
 
+//A point is some char array consisting of 4 characters {H,H,M,M}
+int calculateNumberOfIntervalsAPointIsIn(char** landingTimes, char** departureTimes, const int N, char* point) {
+	int numberOfIntervals = 0;
+	for (int j = 0; j < N; j++) {
+		if (pointIsInRange(point, landingTimes[j], departureTimes[j])) {
+			numberOfIntervals++;
+		}
+	}
+	return numberOfIntervals;
+}
+
+bool pointIsInRange(char* point, char* landingTime, char* departureTime) {
+	return (compareTimes(point, landingTime) >= 0 && compareTimes(point, departureTime) <= 0);
+}
+
+//Now that we know the maximal number of intersecting intervals, we should print all of them 
+void evaluateMaxIntersectionIntervals(char** landingTimes, char** departureTimes, const int N, const int maxIntersectingCount) {
+	//For each point, we evaluate the number of intervals it is in. If the number is equal to maxIntersectingCount, 
+	//it is the start of the next biggest intersection. 
+	
+	char** starts = new char*[N]();
+	char** ends = new char*[N]();
+
+	int startsInd = 0, endsInd = 0;
+
+	for (int i = 0; i < N; i++) {
+		int intervalsLandingTimeIsIn = calculateNumberOfIntervalsAPointIsIn(landingTimes, departureTimes, N, landingTimes[i]);
+		int intervalsDepartureTimeIsIn = calculateNumberOfIntervalsAPointIsIn(landingTimes, departureTimes, N, departureTimes[i]);
+
+		if (intervalsLandingTimeIsIn == maxIntersectingCount && !isInArray(landingTimes[i], starts, N)) {
+			starts[startsInd++] = landingTimes[i];
+		}
+		if (intervalsDepartureTimeIsIn == maxIntersectingCount && !isInArray(departureTimes[i], ends, N)) {
+			ends[endsInd++] = departureTimes[i];
+		}
+	}
+
+	for (int i = 0; i < startsInd; i++) {
+		printTime(starts[i]);
+		std::cout << '-';
+		printTime(ends[i]);
+		std::cout << '\n';
+	}
+
+	delete[]starts;
+	delete[]ends;
+}
+
+void printTime(char* time) {
+	for (int i = 0; i < HHMM; i++) {
+		std::cout << time[i];
+	}
+}
+
+bool isInArray(char* element, char** arr, const int N) {
+	for (int i = 0; i < N; i++) {
+		if (arr[i] == nullptr) return false;
+		if (compareTimes(element, arr[i]) == 0) return true;
+	}
+	return false;
+}
